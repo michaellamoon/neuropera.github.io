fe7f39167f7684ac3131bf83f0a2d01eccbe690b
neuropera <82995027+neuropera@users.noreply.github.com>
2021-05-06 12:14:09 -0700

    Update counterscript.js

--- counterscript.js
+++ counterscript.js
@@ -1,47 +1,39 @@
-(function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === "complete") {
+      var clockdiv = document.getElementsByClassName("clockdiv");
+    var countDownDate = new Array();
+      for (var i = 0; i < clockdiv.length; i++) {
+          countDownDate[i] = new Array();
+          countDownDate[i]['el'] = clockdiv[i];
+          countDownDate[i]['time'] = new Date(clockdiv[i].getAttribute('data-date')).getTime();
+          countDownDate[i]['days'] = 0;
+          countDownDate[i]['hours'] = 0;
+          countDownDate[i]['seconds'] = 0;
+          countDownDate[i]['minutes'] = 0;
+      }
+    
+    var countdownfunction = setInterval(function() {
+for (var i = 0; i < countDownDate.length; i++) {
+              var now = new Date().getTime();
+              var distance = countDownDate[i]['time'] - now;
+               countDownDate[i]['days'] = Math.floor(distance / (1000 * 60 * 60 * 24));
+               countDownDate[i]['hours'] = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+               countDownDate[i]['minutes'] = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+               countDownDate[i]['seconds'] = Math.floor((distance % (1000 * 60)) / 1000);
+              
+               if (distance < 0) {
+                  countDownDate[i]['el'].querySelector('.days').innerHTML = 0;
+              countDownDate[i]['el'].querySelector('.hours').innerHTML = 0;
+              countDownDate[i]['el'].querySelector('.minutes').innerHTML = 0;
+              countDownDate[i]['el'].querySelector('.seconds').innerHTML = 0;
+               }else{
+countDownDate[i]['el'].querySelector('.days').innerHTML = countDownDate[i]['days'];
+              countDownDate[i]['el'].querySelector('.hours').innerHTML = countDownDate[i]['hours'];
+              countDownDate[i]['el'].querySelector('.minutes').innerHTML = countDownDate[i]['minutes'];
+              countDownDate[i]['el'].querySelector('.seconds').innerHTML = countDownDate[i]['seconds'];
+}
 
-  let dday = "May 7, 2021 19:00:00",
-      countDown = new Date(dday).getTime(),
-      x = setInterval(function() {
-
-        let now = new Date().getTime(),
-            distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-          clearInterval(x);
-
-          let headline = document.getElementById("headline");
-          headline.innerText = "Join our live zoom performances!";
-
-
-          var theoriginal = document.getElementById('ori');
-          var thereplacement = document.createElement('a');
-            var att = document.createAttribute("style");
-            att.value = "font-size: 1.3vw"
-            thereplacement.setAttributeNode(att);
-          thereplacement.appendChild(document.createTextNode('https://calarts.zoom.us/j/95221880109?pwd=RDVXZGJRTkpMbms1YTRINUVib09xdz09'));
-          thereplacement.href = "https://calarts.zoom.us/j/8728462581";
-            var att2 = document.createAttribute("target");
-            att2.value = "_blank";
-            thereplacement.setAttributeNode(att2);
-          theoriginal.replaceChild(thereplacement, theoriginal.lastChild);
-
-
-          countdown.style.display = "none";
-          content.style.display = "block";
-
-
-        }
-        //seconds
-      }, 0)
-  }());
+   }
+          }, 1000);
+  }
+});
